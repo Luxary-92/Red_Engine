eb6f5409495fff62a8f3538d45a4d71ed5f041df
Luxary-92 <79161171+Luxary-92@users.noreply.github.com>
2023-12-07 00:21:13 +0100

    GameObject Update

--- Red_Engine/GameObject.cpp
+++ Red_Engine/GameObject.cpp
@@ -1,17 +1,31 @@
 #include "GameObject.h"
 #include "Component.h"
+#include "ModuleInput.h"
 
 #include "Comp_Transform.h"
 #include "Comp_Mesh.h"
 #include "Comp_Material.h"
 
-GameObject::GameObject()
+GameObject::GameObject(std::string name, GameObject* parent): active(true), destroy(false), parent(nullptr)
 {
-
-
+	if (parent != nullptr)
+		parent->Children.push_back(this);
 }
 
-bool GameObject::isActive()
+const std::vector<GameObject*>& GameObject::GetChildren() const {
+	return Children;
+}
+
+GameObject::~GameObject()
+{
+	for (size_t i = 0; i < Children.size(); i++)
+	{
+		Children[i] = nullptr;
+	}
+	Children.clear();
+}
+
+bool GameObject::Active()
 {
 	return active;
 }
@@ -29,3 +43,17 @@ void GameObject::Disable()
 	active = false;
 
 }
+
+//GameObject Componets
+Comp_Material* GameObject::Get_Comp_Material() {
+
+}
+
+Comp_Mesh* GameObject::Get_Comp_Mesh() {
+
+}
+
+Comp_Transform* GameObject::Get_Comp_Transform() {
+
+}
+
--- Red_Engine/GameObject.h
+++ Red_Engine/GameObject.h
@@ -1,19 +1,37 @@
 #include "Application.h"
 #include "Component.h"
+#include "Module.h"
+#pragma once
+#include <vector>
+#include <string>
+
+
+class Comp_Material;
+class Comp_Mesh;
+class Comp_Transform;
 
 class GameObject
 {
 public:
 
-	GameObject();
-	GameObject(GameObject* parent);
+	GameObject(std::string Object_Name,GameObject* parent);
 	~GameObject();
 
-	bool isActive();
+	bool Active();
 	void Enable();
 	void Disable();
 
 	bool active;
+	bool destroy;
+
+	std::string Object_Name;
 
 	GameObject* parent;
+	const std::vector<GameObject*>& GetChildren() const;
+	std::vector<Component*> Components;
+	std::vector<GameObject*> Children;
+
+	Comp_Material* Get_Comp_Material();
+	Comp_Mesh* Get_Comp_Mesh();
+	Comp_Transform* Get_Comp_Transform();	
 };
