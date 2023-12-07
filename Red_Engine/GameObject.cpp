7cc13c510c91f87f6aa5877bb446155bd4f6d9e5
Luxary-92 <79161171+Luxary-92@users.noreply.github.com>
2023-12-07 23:31:19 +0100

    Parent Childen Funcions

--- Red_Engine/GameObject.cpp
+++ Red_Engine/GameObject.cpp
@@ -6,35 +6,52 @@
 #include "Comp_Mesh.h"
 #include "Comp_Material.h"
 
-GameObject::GameObject() 
+GameObject::GameObject(std::string name)
 {
-	Object_Name = "GameObject";
-	parent = nullptr;
+	this->NAME = name;
+
+	PARENT = nullptr;
+
+	transform = new Comp_Transform(this);
+	Components.push_back(transform);
+
 	destroy = false;
 
 }
 
-GameObject::GameObject(GameObject* Object_parent)
+GameObject::GameObject(std::string name,GameObject* Object_parent)
 {
-	Object_Name = "GameObject";
-	this->parent = Object_parent;
+	this->NAME = name;
+
+	this->PARENT = Object_parent;
 	destroy = false;
 
-	if (Object_parent != nullptr)
+	if (Object_parent != nullptr) {
 		Object_parent->Children.push_back(this);
+	}
+
+	transform = new Comp_Transform(this);
+	Components.push_back(transform);
 }
 
-const std::vector<GameObject*>& GameObject::GetChildren() const {
-	return Children;
-}
 
 GameObject::~GameObject()
 {
+	transform = nullptr;
+
+	for (size_t i = 0; i < Components.size(); ++i)
+	{
+		delete Components[i];
+		Components[i] = nullptr;
+	}
+	Components.clear();
+
 	for (size_t i = 0; i < Children.size(); i++)
 	{
 		Children[i] = nullptr;
 	}
-	Children.clear();
+
+
 }
 
 bool GameObject::Active()
@@ -46,14 +63,62 @@ void GameObject::Enable()
 {
 	active = true;
 
-	if (parent != nullptr)
-		parent->Enable();
+	if (PARENT != nullptr)
+		PARENT->Enable();
 }
 
 void GameObject::Disable()
 {
 	active = false;
 
+}
+
+void GameObject::Update()
+{
+	// No tots els children o components tenen un update
+	for (size_t i = 0; i < Children.size(); ++i)
+	{
+		Children[i]->Update();
+	}
+
+	for (size_t i = 0; i < Components.size(); ++i)
+	{
+		Components[i]->Update();
+	}
+}
+
+void GameObject::AddComponent(Component* component)
+{
+	component->Object = this;
+	Components.push_back(component);
+}
+
+//Hierarchy
+void GameObject::SetParent(GameObject* parent) {
+	PARENT = parent;
+
+}
+
+void GameObject::DeleteChild(GameObject* child) {
+
+	for (size_t i = 0; i < Children.size(); i++)
+	{
+		if (child == Children[i]) {
+			Children.erase(Children.begin() + i);
+		}
+	}
+	child->PARENT = nullptr;
+}
+
+void GameObject::AddChild(GameObject* child) {
+
+	if (child->PARENT != nullptr) {
+		child->PARENT->DeleteChild(child);
+	}
+
+	child->SetParent(this);
+
+	Children.push_back(child);
 }
 
 //GameObject Componets
@@ -79,6 +144,7 @@ Comp_Transform* GameObject::Get_Comp_Transform() {
 	return nullptr;
 }
 
+//Editor
 void GameObject::Inspector_Editor() {
 
 	for (size_t i = 0; i < Components.size(); i++)
--- Red_Engine/GameObject.h
+++ Red_Engine/GameObject.h
@@ -13,13 +13,16 @@ class GameObject
 {
 public:
 
-	GameObject();
-	GameObject(GameObject* Object_parent);
+	GameObject(std::string name);
+	GameObject(std::string name,GameObject* Object_parent);
 	~GameObject();
+
+	std::string NAME;
 
 	bool Active();
 	void Enable();
 	void Disable();
+	void Update();
 
 	void Inspector_Editor();
 
@@ -27,13 +30,21 @@ public:
 	bool destroy;
 
 	std::string Object_Name;
-
-	GameObject* parent;
-	const std::vector<GameObject*>& GetChildren() const;
+	
+	void AddComponent(Component* component);
 	std::vector<Component*> Components;
+
+	// Hierarchy
+	GameObject* PARENT;
+	void SetParent(GameObject* parent);
+	void AddChild(GameObject* child);
+	void DeleteChild(GameObject* child);
+
 	std::vector<GameObject*> Children;
 
 	Comp_Material* Get_Comp_Material();
 	Comp_Mesh* Get_Comp_Mesh();
-	Comp_Transform* Get_Comp_Transform();	
+	Comp_Transform* Get_Comp_Transform();
+	Comp_Transform* transform;
+	/*Comp_Camera* Get_Comp_amera();*/
 };
